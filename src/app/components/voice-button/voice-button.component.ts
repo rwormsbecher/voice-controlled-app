c2864f9d09fd3011f165985957528392bb827a2b
Peter Eijgermans <peter.eijgermans@ka.prorail.nl>
2025-05-28 15:04:03 +0200

    refactored

--- src/app/components/voice-button/voice-button.component.ts
+++ src/app/components/voice-button/voice-button.component.ts
@@ -1,9 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { VoiceControlService } from '../../services/voice-control.service';
 import { MatIcon } from '@angular/material/icon';
 import { MatButton } from '@angular/material/button';
+import { MatSnackBar } from '@angular/material/snack-bar';
 import { LanguageSelectorComponent } from "../language-selector/language-selector.component";
+import { Subject, takeUntil } from 'rxjs';
 
 @Component({
   selector: 'app-voice-button',
@@ -12,27 +14,64 @@ import { LanguageSelectorComponent } from "../language-selector/language-selecto
   templateUrl: './voice-button.component.html',
   styleUrls: ['./voice-button.component.scss']
 })
-export class VoiceButtonComponent {
+export class VoiceButtonComponent implements OnInit, OnDestroy {
+  private destroy$ = new Subject<void>();
+  isListening = false;
+
   constructor(
     private voiceService: VoiceControlService,
-    private router: Router
+    private router: Router,
+    private snackBar: MatSnackBar
   ) {}
 
+  ngOnInit() {
+    // Subscribe to errors
+    this.voiceService.error$
+      .pipe(takeUntil(this.destroy$))
+      .subscribe(error => {
+        if (error) {
+          this.isListening = false;
+          this.snackBar.open(error, 'Close', {
+            duration: 5000,
+            horizontalPosition: 'center',
+            verticalPosition: 'bottom'
+          });
+        }
+      });
+  }
+
+  ngOnDestroy() {
+    this.destroy$.next();
+    this.destroy$.complete();
+    this.voiceService.stopListening();
+  }
+
   listen() {
-   this.voiceService.startListening();
-    this.voiceService.command$.subscribe((command: string) => {
-      console.log('started listening on voice button')
-      console.log('Voice command:', command);
+    this.isListening = !this.isListening;
+    
+    if (this.isListening) {
+      this.voiceService.startListening();
+      this.voiceService.command$
+        .pipe(takeUntil(this.destroy$))
+        .subscribe({
+          next: (command: string) => {
+            console.log('Voice command:', command);
 
-      if (command.includes('home')) {
-        this.router.navigate(['/home']);
-      } else if (command.includes('about')) {
-        this.router.navigate(['/about']);
-      } else if (command.includes('contact')) {
-        this.router.navigate(['/contact']);
-      } 
-
-      console.log('command to log', command)
-    });
+            if (command.includes('home')) {
+              this.router.navigate(['/home']);
+            } else if (command.includes('about')) {
+              this.router.navigate(['/about']);
+            } else if (command.includes('contact')) {
+              this.router.navigate(['/contact']);
+            }
+          },
+          error: (error) => {
+            console.error('Command error:', error);
+            this.isListening = false;
+          }
+        });
+    } else {
+      this.voiceService.stopListening();
+    }
   }
 }
